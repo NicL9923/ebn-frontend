dd699eaba49354f51dbe0fafe8b5203140b0d9d1
Nic9923 <nicl9923@gmail.com>
2020-08-23 14:12:41 -0500

    Added other pages.

---
+++ pages/blog.js
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+
+export default function Blog() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Explorers by Nature</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Explorers by Nature</h1>
+      </main>
+
+      <footer>
+        <span>Copyright © 2020 Nicolas Layne</span>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
