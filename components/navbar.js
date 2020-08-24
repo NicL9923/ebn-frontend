4d6c6e25fcc77a7c7e5ef02c03c2521961ba8895
Nic9923 <nicl9923@gmail.com>
2020-08-24 13:07:15 -0500

    Finish navbar and further homepage layout work.

--- components/navbar.js
+++ components/navbar.js
@@ -6,15 +6,14 @@ class Navbar extends React.Component {
         return(
             <nav className="navbar">
                 <ul>
-                    <li>
-                        <img className="logo" src="/EbnLogo.svg"/>
-                        <Link href="/"><a>Explorers By Nature</a></Link>
-                    </li>
-                    <li><Link href="/podcast"><a>Podcast</a></Link></li>
-                    <li><Link href="/blog"><a>Blog</a></Link></li>
-                    <li><Link href="/store"><a>Store</a></Link></li>
-                    <li><Link href="/aboutus"><a>About Us</a></Link></li>
-                    <li><Link href="/contactus"><a>Contact Us</a></Link></li>
+                    <li><img className="logo" src="/EbnLogo.svg" alt="logo"/></li>
+                    <li><Link href="/"><a className="navLink" style={{fontWeight: "bold"}}>Explorers By Nature</a></Link></li>
+
+                    <li><Link href="/podcast"><a className="navLink">Podcast</a></Link></li>
+                    <li><Link href="/blog"><a className="navLink">Blog</a></Link></li>
+                    <li><Link href="/store"><a className="navLink">Store</a></Link></li>
+                    <li><Link href="/aboutus"><a className="navLink">About Us</a></Link></li>
+                    <li><Link href="/contactus"><a className="navLink">Contact Us</a></Link></li>
                 </ul>
 
                 <style jsx>{`
@@ -25,15 +24,29 @@ class Navbar extends React.Component {
 
                     .navbar ul {
                         list-style-type: none;
+                        display: flex;
+                        flex-direction: row;
+                        align-items: center;
                     }
 
                     .navbar ul li {
-                        display: inline-block;
+                        display: block;
                         padding: 5px 10px;
                     }
 
-                    .logo {
+                    .navLink {
+                        color: #fafafa;
+                        text-decoration: none;
+                        transition: ease 0.3s;
+                        font-size: 18px;
+                    }
 
+                    .navLink:hover {
+                        color: #ababab;
+                    }
+
+                    .logo {
+                        filter: invert(95%) sepia(41%) saturate(1%) hue-rotate(219deg) brightness(110%) contrast(84%);
                     }
                 `}</style>
             </nav>
--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,54 @@ export default function Home() {
 
       <Navbar/>
 
+      <div className="header">
+        <div>
+          <h1>Mankind was born to stay curious and wander.</h1>
+          <button>
+            <i class="fa fa-podcast" aria-hidden="true"></i>
+            <span>Life's Next Journey</span>
+          </button>
+        </div>
+
+        <img src="nowhere" alt="The creators in nature"/>
+      </div>
+
+      <div className="picsOfUs">
+        <div>
+          <img src="ourPics" alt="Chandler"/>
+          <h3>Chandler</h3>
+        </div>
+
+        <div>
+          <img src="ourPics" alt="Nicole"/>
+          <h3>Nicole</h3>
+        </div>
+
+        <div>
+          <img src="ourPics" alt="Kimberly"/>
+          <h3>Kimberly</h3>
+        </div>
+
+        <div>
+          <img src="ourPics" alt="Nicolas"/>
+          <h3>Nicolas</h3>
+        </div>
+      </div>
+
       <footer>
         <span>Copyright © 2020 Nicolas Layne</span>
       </footer>
 
       <style jsx>{`
-        asd
+        .header {
+          display: grid;
+          grid-template-columns: 25% auto;
+        }
+
+        .picsOfUs {
+          display: grid;
+          grid-template-columns: auto auto auto auto;
+        }
       `}</style>
 
       <style jsx global>{`
